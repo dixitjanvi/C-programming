de86ea6e552999eff8c7252a4a52e63d75d3e4df
JANVI DIXIT <dixitjanvi214@gmail.com>
2026-02-09 23:52:52 +0530

    Greeting_Program.c

---
+++ Functions/Simple_function(No parameter,No return)/Greeting_Program.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void greet(){           //function name-return type(void)
+    printf("Hey good morning\n");
+    printf("How are you?");
+}
+int main(){             //return type
+    printf("Hii, I am Janvi Dixit\n");
+    greet();            //calling the function
+    return 0;
+}
