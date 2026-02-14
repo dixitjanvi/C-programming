ff2d8dcfb2cd460e242cc3a17dc81b612f7d7aaa
JANVI DIXIT <dixitjanvi214@gmail.com>
2026-02-14 21:35:07 +0530

    Print_1_to_n(x, n).c

---
+++ Recursion/Recursion with parameter/Print_1_to_n(x, n).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void print(int x,int n){
+    if(x>n) return;
+    printf("%d\n",x);
+    print(x+1,n);
+}
+int main(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    print(1,n);
+    return 0;
+}
