c8373513390a6804af964db9c405505c30dbf721
JANVI DIXIT <dixitjanvi214@gmail.com>
2026-02-15 02:26:13 +0530

    Store 10 at every index (5×5).c

---
+++ Arrays_2D/Initialize_Array/fill_matrix_10.c
@@ -0,0 +1,17 @@
+//WAP to store 10 at every index of a 2D matrix with 5 rows and 5 columns.
+#include<stdio.h>
+int main(){
+    int arr[5][5];
+    for(int i=0; i<5;  i++){
+        for(int j=0; j<5; j++){
+            arr[i][j]=10;
+        }
+    }
+    for(int i=0; i<5;  i++){
+        for(int j=0; j<5; j++){
+            printf("%d ",arr[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
