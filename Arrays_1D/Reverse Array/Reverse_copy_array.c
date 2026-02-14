0c700ba4d7032bfb8e5d47c20564d60aa4362c9a
JANVI DIXIT <dixitjanvi214@gmail.com>
2026-02-15 02:02:11 +0530

    Reverse_copy_array.c

---
+++ Arrays_1D/Reverse Array/Reverse_copy_array.c
@@ -0,0 +1,19 @@
+//Write a program to copy the contents of one array into another in the reverse order.
+#include<stdio.h>
+int main(){
+    int arr[]={1,2,3,4,5,6,7};
+    int n= sizeof(arr)/4;
+    for(int i=0; i<n; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    int brr[n];
+    for(int i=0; i<n; i++){
+        brr[i]=arr[n-1-i];
+    }
+    for(int i=0; i<n; i++){
+        printf("%d ",brr[i]);
+    }
+    printf("\n");
+    return 0;
+}
