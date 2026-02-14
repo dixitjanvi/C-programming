a9db6fc0d8d728f63542c174105eceeda65231e9
JANVI DIXIT <dixitjanvi214@gmail.com>
2026-02-14 22:20:05 +0530

    Array_initialization.c

---
+++ Arrays_1D/Basic_Array/Array_initialization.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int arr[5], x=1, y=8, z=5;
+    float a[3] = {3.14,9.8,2.81};
+    for(int i=0; i<3; i++){      //output
+        printf("%f ",a[i]);
+    }
+    //int size = 10, b[size];
+    //int b [size], size = 10; //it's wrong
+    return 0;
+}
