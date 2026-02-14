ae8736f3c40be78b48def2481a0e4a89ee902efc
JANVI DIXIT <dixitjanvi214@gmail.com>
2026-02-14 19:44:37 +0530

    Factorial_return.c

---
+++ Functions/Function with Return type/Factorial_return.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int fact(int n){
+    int f = 1;
+    for(int i=1; i<=n; i++){
+        f += i;
+    }
+    return f;
+}
+
+int ncr(int n,int r){
+    retrun fact(n)/(fact(r)*fact(n-r));
+}
+
+int main(){
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    int r;
+    printf("Enter r:");
+    scanf("%d",&r);
+    
+    int ans = ncr(n,r);
+    printf("%d",ans);
+    return 0;
+}
+
