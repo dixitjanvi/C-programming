364a728b24ba4b8054de2944619baede313d5f78
JANVI DIXIT <dixitjanvi214@gmail.com>
2026-02-17 01:17:01 +0530

    Boiler_plate.c

---
+++ Structure/Basics/Boiler_plate.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x;     //declaration
+    x = 5;     //initialization
+    struct student{     //Forming of user define data type
+        int roll;
+        float per;
+        char grade;
+    };
+    struct student s1; //declaration
+    //code
+  return 0;
+}
