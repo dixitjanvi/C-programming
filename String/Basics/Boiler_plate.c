68c235b4fb100b27b033691ec2cc3b74aec0e027
JANVI DIXIT <dixitjanvi214@gmail.com>
2026-02-17 01:18:48 +0530

    Boiler_plate.c

--- String/Basics/Boiler_plate.c
+++ String/Basics/Boiler_plate.c
@@ -1,16 +1,9 @@
-//Structures- It's a user define data type
-//boiler plate
-/*#include<stdio.h>
+#include<stdio.h>
+#include<string.h>
+
 int main(){
-    int x;     //declaration
-    x = 5;     //initialization
-    struct student{     //Forming of user define data type
-        int roll;
-        float per;
-  struct student s1; //declaration
-    s1.roll=96;
-    s1.per=97.4;
-    printf("%d\n",s1.roll);
-    printf("%f\n",s1.per);
-return 0;
-};
+    char str[50];    
+    scanf("%s", str);   
+    printf("%s", str); 
+    return 0;
+}
